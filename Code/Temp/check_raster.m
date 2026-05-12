15e635f836910304f8a7d81650ef410163eff8f1
HeptaFang <septafang@gmail.com>
2026-05-12 14:56:16 -0400

    Temp script for fig 1b

---
+++ Code/Temp/check_raster.m
@@ -0,0 +1,13 @@
+clear;
+%% Get root folder
+code_depth = 3;
+script_path = mfilename('fullpath');
+root = script_path;
+for i = 1:code_depth
+    root = fileparts(root);
+end
+% include code folder and utils
+addpath(fileparts(script_path));
+addpath(fullfile(root, 'Code', 'Utils'));
+
+%% Main
